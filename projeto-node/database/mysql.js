83ec4393b1ff87cc3cc279e5a2ec284d298f0555
Joao Vitor <joaovitorramalho13@gmail.com>
2025-07-02 19:52:59 -0300

    Alterando bd

--- projeto-node/database/mysql.js
+++ projeto-node/database/mysql.js
@@ -1,13 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: 'localhost',
-    port: '3306',
-    username: 'root',
-    password: 'root',
-    database: 'pratica_node'
-});
-
+// const sequelize = new Sequelize({
+//     dialect: 'mysql',
+//     host: 'localhost',
+//     port: '3306',
+//     username: 'root',
+//     password: 'root',
+//     database: 'pratica_node'
+// });
+const sequelize = new Sequelize('<postgresql://pratica_filmes_user:EsmGtJDBgslv50XCI9VUyOGZSkkMBtVI@dpg-d1ir7l2li9vc73bsk6vg-a/pratica_filmes_db_8v7b>');
 
 export default sequelize;
